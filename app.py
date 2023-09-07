ff4c86675f8ecb55fce44a1aef814bd935c59fb9
CormacDaly <cormacdaly24@gmail.com>
2023-09-07 14:14:10 +0100

    chartsjs add, basic layout created with bootstrap

--- app.py
+++ app.py
@@ -25,7 +25,16 @@ def gdp_test():
         for entry in gdp:
             print(entry)
     return render_template('gdp_test.html', gdp=gdp)
+
+
+@app.route('/chartjs_test')
+def chartjs_test():
+    return render_template('chartjs_test.html')
         
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    template_files = [os.path.join('templates', f) for f in os.listdir('templates') if f.endswith('.html')]
+    app.run(debug=True, extra_files=template_files)
+
